59beec13bb073f9ab133e2f68753fcb193245a82
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2011-08-06 07:25:02 -0700

    Updates to scripts so that paths work out properly.

--- scripts/weekly.sh
+++ scripts/weekly.sh
@@ -1,5 +1,9 @@
 #!/usr/local/bin/bash
 
+export PYTHONPATH=/home/glick/lib/python
+
+export PATH=/home/glick/bin:/home/glick/bin/git/bin:/home/glick/bin/svm:/usr/local/bin:$PATH
+
 cd /home/glick/AINews/code
 date >> ../logs/log.txt 2>&1
 python AINews.py all >> ../logs/log.txt 2>&1
