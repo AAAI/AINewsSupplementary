8eb49d9f15183924f70e429750784762886bf6c5
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2011-10-09 09:27:19 -0700

    Whitelist updates and scripts updates.

--- scripts/weekly.sh
+++ scripts/weekly.sh
@@ -4,6 +4,8 @@ export PYTHONPATH=/home/glick/lib/python
 
 export PATH=/home/glick/bin:/home/glick/bin/git/bin:/home/glick/bin/svm:/usr/local/bin:$PATH
 
+cp /home/glick/html/semiauto_email-blank.html /home/glick/html/semiauto_email.html
+rm /home/glick/NewsFinder/output/email_output.txt
 cd /home/glick/NewsFinder/code
 date >> ../logs/log.txt 2>&1
 python AINews.py crawl >> ../logs/log.txt 2>&1
