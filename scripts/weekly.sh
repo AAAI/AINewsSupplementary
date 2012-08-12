26f56fb6aa8d0c883854842b6d9fd8fd436d9d3d
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2012-08-12 05:28:03 -0700

    Various updates for new server.

--- scripts/weekly.sh
+++ scripts/weekly.sh
@@ -1,15 +1,9 @@
-#!/usr/local/bin/bash
+#!/bin/bash
 
-export PYTHONPATH=/home/glick/lib/python
-
-export PATH=/home/glick/bin:/home/glick/bin/git/bin:/home/glick/bin/svm:/usr/local/bin:$PATH
-
-cp /home/glick/html/semiauto_email-blank.html /home/glick/html/semiauto_email.html
-rm /home/glick/NewsFinder/output/email_output.txt
-cd /home/glick/NewsFinder/code
+cp /var/www/wiki/html/semiauto_email-blank.html /var/www/wiki/html/semiauto_email.html
+rm /home/aitopicsadmin/NewsFinder/output/email_output.txt
+cd /home/aitopicsadmin/NewsFinder/code
 date >> ../logs/log.txt 2>&1
 python AINews.py crawl >> ../logs/log.txt 2>&1
 python AINews.py prepare >> ../logs/log.txt 2>&1
 echo "AINewsFinder Finished!" >> ../logs/log.txt 2>&1
-
-
