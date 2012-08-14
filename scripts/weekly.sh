69964c462cb1a27129c0fa3c900f6b40a246de08
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2012-08-13 18:32:38 -0700

    Modified location mentioned in script.

--- scripts/weekly.sh
+++ scripts/weekly.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cp /var/www/wiki/html/semiauto_email-blank.html /var/www/wiki/html/semiauto_email.html
-rm /home/aitopicsadmin/NewsFinder/output/email_output.txt
-cd /home/aitopicsadmin/NewsFinder/code
+rm /home/aitopicsweb/NewsFinder/output/email_output.txt
+cd /home/aitopicsweb/NewsFinder/code
 date >> ../logs/log.txt 2>&1
 python AINews.py crawl >> ../logs/log.txt 2>&1
 python AINews.py prepare >> ../logs/log.txt 2>&1
