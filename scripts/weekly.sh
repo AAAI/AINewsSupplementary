6beae7baef1a5268b80bb31ff887e80bb3493e47
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-05-25 09:54:25 -0700

    Removed weekly email script (rewrote script, not putting in version control).

--- scripts/weekly.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp /var/www/wiki/html/semiauto_email-blank.html /var/www/wiki/html/semiauto_email.html
-rm /home/aitopicsweb/NewsFinder/output/email_output.txt
-cd /home/aitopicsweb/NewsFinder/code
-date >> ../logs/log.txt 2>&1
-python AINews.py crawl >> ../logs/log.txt 2>&1
-python AINews.py prepare >> ../logs/log.txt 2>&1
-echo "AINewsFinder Finished!" >> ../logs/log.txt 2>&1
